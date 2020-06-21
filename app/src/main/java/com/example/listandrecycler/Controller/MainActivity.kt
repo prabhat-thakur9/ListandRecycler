2ee5783234c1073f47130bb5ac989bea34a560fa
Prabhat Thakur <thakurprabhat2012@gmail.com>
2020-06-21 12:09:23 +0530

    Lesson 01

--- app/src/main/java/com/example/listandrecycler/Controller/MainActivity.kt
+++ app/src/main/java/com/example/listandrecycler/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.listandrecycler
+package com.example.listandrecycler.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listandrecycler.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
